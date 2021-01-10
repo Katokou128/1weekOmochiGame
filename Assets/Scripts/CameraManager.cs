be5f861d56b71482a34b65eaf150b76d5e6ea5eb
katokou <77188960+Katokou128@users.noreply.github.com>
2021-01-11 02:16:49 +0900

    最初のコミット

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 mainYposition;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        mainYposition = mainCamera.transform.position; //メインカメラの最初のY軸の位置を保持
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        Vector3 position = player.transform.position; // プレイヤーの位置をposition変数にいれる
+
+        position.z = -10; // position変数のzを-10にする(カメラの位置を手前にする)
+        position.y = mainYposition.y; // position変数のyを最初のメインカメラのY軸の高さにする
+
+        mainCamera.transform.position = position; //カメラの位置をプレイヤーの位置にする
+
+        
+        if(position.x < 0.0075)
+        {
+            position.x = 0.0075f;
+            mainCamera.transform.position = position;
+        }
+        
+        if (position.x > 35.4)
+        {
+            position.x = 35.4f;
+            mainCamera.transform.position = position;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    float direction = 0f; //プレイヤーが移動する向き
+    public float moveSpeed = 3f; //プレイヤーの移動スピード
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 動いていない時
+        direction = 0f;
+
+        // 右に移動する時
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = moveSpeed;
+            //Debug.Log("右移動");
+        }
+
+        // 左に移動する時
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -moveSpeed;
+            //Debug.Log("左移動");
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //プレイヤーを動かす処理
+        rbody.velocity = new Vector2(direction, rbody.velocity.y);
+    }
+}
