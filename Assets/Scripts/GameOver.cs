f43e38842953560c75f1046228f69779fea200f8
katokou <77188960+Katokou128@users.noreply.github.com>
2021-01-11 22:22:46 +0900

    ゲームオーバーシーンの作成など

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // ゲームオーバーシーンのボタン設定
+
+
+    // リトライボタンを押したとき
+    public void OnClickRetryButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
---
+++ Assets/Scripts/Kine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kine : MonoBehaviour
+{
+    // 敵キャラ（きね）のスクリプト
+
+    Rigidbody2D rbody;
+    public float moveSpeed = -3;
+    public string player;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rbody.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //ぶつかったら逆へ移動する
+        moveSpeed = -moveSpeed;
+
+        //移動している方向の向きに画像を合わせる
+        if (moveSpeed > 0)
+        {
+            transform.localScale = new Vector3(-0.25f, 0.25f, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.25f, 0.25f, 1);
+        }
+
+        // プレイヤーとぶつかったらシーン移動
+        if(collision.gameObject.name == player)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
